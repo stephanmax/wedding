6668cb601de6895ff59bc01b9f7329b38aaf5982
Stephan Max <hello@stephanmax.com>
2019-02-15 06:11:44 +0100

    First function test

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -1,4 +1,30 @@
+const Https = require('https')
+
 exports.handler = (event, context, callback) => {
-  const body = JSON.parse(event.body).payload
-  console.log(body)
+  const {form_name, data} = JSON.parse(event.body).payload
+  console.log(context)
+  
+  if (form_name === 'geschenke') {
+    const options = {
+      host: 'api.github.com',
+      path: 'GET /repos/stephanmax/wedding/contents/geschenke.json',
+      headers: {   
+        'Authorization': 'Basic ' + new Buffer(`${context.GITHUB_USERNAME}:${context.GITHUB_TOKEN}`).toString('base64')
+      }   
+   }
+   
+    https.get(options, res => {
+      var body = '';
+    
+      res.on('data', data => {
+        body += data
+      });
+    
+      res.on('end', () => {
+        console.log(body);
+      })
+    
+      res.on('error', callback);
+    });
+  }
 }
