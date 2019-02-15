13983b58338df2743f58ba3eb985ad01a00590eb
Stephan Max <hello@stephanmax.com>
2019-02-15 06:35:18 +0100

    Fix request path

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -3,11 +3,13 @@ const Https = require('https')
 exports.handler = (event, context, callback) => {
   const {form_name, data} = JSON.parse(event.body).payload
   const {GITHUB_USERNAME, GITHUB_TOKEN} = process.env
+  console.log(GITHUB_USERNAME)
+  console.log(GITHUB_TOKEN)
   
   if (form_name === 'geschenke') {
     const options = {
       host: 'api.github.com',
-      path: 'GET /repos/stephanmax/wedding/contents/geschenke.json',
+      path: '/repos/stephanmax/wedding/contents/geschenke.json',
       headers: {   
         'Authorization': 'Basic ' + new Buffer(`${GITHUB_USERNAME}:${GITHUB_TOKEN}`).toString('base64')
       }   
