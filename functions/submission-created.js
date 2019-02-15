737457382476ff51b4e40a8825fdbe741f3fd966
Stephan Max <hello@stephanmax.com>
2019-02-15 06:28:34 +0100

    Fix https call and env vars

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -2,18 +2,18 @@ const Https = require('https')
 
 exports.handler = (event, context, callback) => {
   const {form_name, data} = JSON.parse(event.body).payload
-  console.log(context)
+  const {GITHUB_USERNAME, GITHUB_TOKEN} = process.env
   
   if (form_name === 'geschenke') {
     const options = {
       host: 'api.github.com',
       path: 'GET /repos/stephanmax/wedding/contents/geschenke.json',
       headers: {   
-        'Authorization': 'Basic ' + new Buffer(`${context.GITHUB_USERNAME}:${context.GITHUB_TOKEN}`).toString('base64')
+        'Authorization': 'Basic ' + new Buffer(`${GITHUB_USERNAME}:${GITHUB_TOKEN}`).toString('base64')
       }   
    }
    
-    https.get(options, res => {
+    Https.get(options, res => {
       var body = '';
     
       res.on('data', data => {
